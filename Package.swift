1826ac0c4344147a269c6a44f857d57f6e9cf86b
tramp <wanggang3@staff.sina.com.cn>
2022-01-24 13:35:52 +0800

    update => no message

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "SDKs",
-    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "CryptoSwift", targets: ["CryptoSwift"]),
